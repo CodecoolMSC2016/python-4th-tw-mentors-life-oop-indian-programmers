8cca74f5e0565e2956f08ef6549e0285639de2ef
Jakubinyi <jakubinyi@gmail.com>
2017-01-17 14:16:31 +0100

    student.py added

--- student.py
+++ student.py
@@ -2,4 +2,14 @@ from person import Person
 
 
 class Student(Person):
-    pass
+
+    def __init__(self, knowledge_level, energy_level):
+        self.knowledge_level = knowledge_level
+        self.energy_level = energy_level
+
+    def create_by_csv(csv_path):
+        my_file = open(csv_path, "r")
+        students = []
+        for line in my_file:
+            students.append(line)
+        my_file.close()
