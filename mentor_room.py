d478089c37802a8b3cb15f53e0b0a5acd5d47bd6
LewaDubz <lestrider@gmail.com>
2017-01-18 14:12:25 +0100

    mentor_room.py added with class and instantiation.

---
+++ mentor_room.py
@@ -0,0 +1,6 @@
+class Mentor_Room():
+    def __init__(self, bfa_running, is_clean, mood_level, window_open):
+        self.bfa_running = bfa_running
+        self.is_clean = is_clean
+        self.mood_level = mood_level
+        self.window_open = window_open
