5455acb511ed0336f4c8becba3dc9452ed82e922
imre_meszesan <imre.meszesan@gmail.com>
2017-01-19 11:30:49 +0100

    created classmethod, added csv_path parameter

--- student.py
+++ student.py
@@ -4,15 +4,17 @@ import csv
 
 class Student(Person):
 
-    def __init__(self, knowledge_level, energy_level):
+    def __init__(self, first_name, last_name, year_of_birth, gender, knowledge_level, energy_level):
         super().__init__(first_name, last_name, year_of_birth, gender)
         self.knowledge_level = knowledge_level
         self.energy_level = energy_level
 
-    def create_by_csv(csv_path):
+    @classmethod
+    def create_by_csv(cls, csv_path):
         students = []
         with open(csv_path, "r") as csvfile:
             csv_file = csv.reader(csvfile)
             for row in csv_file:
                 student = Student(row[0], row[1])
                 students.append(student)
+        return students
