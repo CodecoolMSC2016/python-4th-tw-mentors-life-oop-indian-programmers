c26ce985d6ad7b683353dd884fff2e64a62c30e1
imremeszesan <imre.meszesan@gmail.com>
2017-01-19 23:21:04 +0100

    added calc_chance_for_fight(), reverted chance_for_pub_fight to integer

--- pub.py
+++ pub.py
@@ -9,7 +9,11 @@ class Pub():
         self.number_of_people = random.randint(1, 30)
         self.music_playing = False
         self.free_tables = bool(random.randint(0, 2))
-        self.chance_for_pub_fight = str(round((self.mood_level / 30) * (self.number_of_people / 5))) + "%"
+        self.chance_for_pub_fight = round((self.mood_level / 30) * (self.number_of_people / 5))
+
+    def calc_chance_for_fight(self):
+        self.chance_for_pub_fight = round(((self.mood_level*10) / 30) * (self.number_of_people / 5))
+        return "Chance for a fight is " + str(self.chance_for_pub_fight) + "%!"
 
     def handle_pub_fight(self):
         self.number_of_people = 0
@@ -21,8 +25,8 @@ class Pub():
         with open("music_list.txt") as f:
             for line in f:
                 music_list.append(line)
-        return random.choice(music_list) + "is playing in the pub."
+        return random.choice(music_list).strip() + " is playing in the pub now."
 
     def serve_alcohol(self):
         self.mood_level += 5
-        return "The overall mood increased in the pub " + str(self.mood_level)
+        return "The overall mood increased to " + str(self.mood_level)
