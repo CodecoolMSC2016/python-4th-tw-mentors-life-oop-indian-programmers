2cb4e576b2cc6e6eec5c0f74800059f31bd8a8dd
imremeszesan <imre.meszesan@gmail.com>
2017-01-19 22:11:55 +0100

    created generator for story strings

--- story.py
+++ story.py
@@ -9,54 +9,74 @@ import os
 import time
 import random
 
+def read_story_from_file(filename):
+    story_list = []
+    with open(filename) as f:
+        for line in f:
+            story_list.append(line)
+    return story_list
+
+def generate_story(story_list):
+    index = 0
+    while index < len(story_list):
+        time.sleep(0.5)
+        yield story_list[index] 
+        index += 1
+
 
 def main():
     cc_msc = CodecoolClass.create_local()
     mentor = random.choice(cc_msc.mentors)
-    print("It's 8 am in the morning, our hero, " + mentor.first_name + " starts his daily routine in the Pub.")
-    time.sleep(1)
-    pub = Pub("Paprika")
-    print("Location: " + pub.name + " Pub")
+    mentor_room = Mentor_Room()
+    bfa_student = random.choice(cc_msc.students)
+    pub = Pub("Paprika Pub")
+    story_list = read_story_from_file("story.txt")
+    story = generate_story(story_list)
+    print(next(story) + mentor.last_name + " " + mentor.first_name, end='')
+    print(next(story), end='')
+    input()
+    print(next(story) + " " +pub.name)
     for key, value in vars(pub).items():
         time.sleep(0.3)
         print("\t" + key + ": " + str(value))
-    print("How many rounds you wish to drink?")
-    number_of_rounds = int(input("\n"))
+    print(next(story))
+    number_of_rounds = int(input(""))
+    alcohol_serving_str = next(story)
     for i in range(number_of_rounds):
-        print("Serving alcohol to people...\n" + pub.serve_alcohol())
+        print( alcohol_serving_str + pub.serve_alcohol())
+        time.sleep(1)
         print(pub.change_music())
-        time.sleep(0.6)
-    print("After so many rounds a bar fight starts,the security comes to handle it")
+        time.sleep(1)
+    print(next(story))
     print(pub.handle_pub_fight())
-    time.sleep(1)
-    print("It's 9AM our hero mentor,has arrived to CodeCool,As soon as he arrives the management calls him")
-    time.sleep(1)
-    print("Management: Dear mentor for your hard and soulful work attitude we have decided to give you a rasise in your salary")
-    time.sleep(1)
+    for counter in range(2):
+        time.sleep(1)
+        print(next(story))
     print(Management.give_raise(mentor))
-    print("Management: Now Be Awesome and teach new stuff to students.")
-    mentor_room = Mentor_Room()
+    input()
+    
+    print(next(story))
     print(mentor_room.start_bfa())
-    bfa_student = random.choice(cc_msc.students)
-    print(bfa_student.last_name + bfa_student.first_name + "starts his/her BFA now.")
     time.sleep(0.5)
-    print(bfa_student.last_name + "turns on his/her laptop.")
+    print(bfa_student.last_name + bfa_student.first_name + next(story))
+    time.sleep(1)
+    print(bfa_student.last_name + next(story))
     laptop = Laptop()
     for msg in laptop.turn_on():
-        os.system("clear")
+        #os.system("clear")
         print(msg)
     for msg in laptop.run_code():
         print(msg)
-    print("Unfortunately, our mentor starts to feel sick because of the heat and he vomits on " +
-          bfa_student.last_name + bfa_student.first_name + ". :(")
+    print(next(story) +
+          bfa_student.last_name + bfa_student.first_name + ".")
     print(mentor_room.gets_dirty())
     print(mentor_room.stop_bfa())
     print(mentor_room.open_window())
     time.sleep(1)
-    print("Management enters mentor room, notices that everything is covered in vomit.\n He fires " +
+    print(next(story) +
           mentor.first_name + mentor.last_name + ".")
     print(Management.fire_mentor(mentor))
-    print(mentor.first_name + mentor.last_name + "wakes up in bed from this nightmare, covered in sweat.\n The end.")
+    print(mentor.first_name + mentor.last_name + next(story))
 
 if __name__ == '__main__':
     main()
