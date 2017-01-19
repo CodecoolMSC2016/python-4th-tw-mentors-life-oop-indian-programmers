5c4d7608c04deba926dab1d32c1a22a4ebf20705
imremeszesan <imre.meszesan@gmail.com>
2017-01-19 22:11:05 +0100

    changed chance_for_pub_fight to a string

--- pub.py
+++ pub.py
@@ -9,7 +9,7 @@ class Pub():
         self.number_of_people = random.randint(1, 30)
         self.music_playing = False
         self.free_tables = bool(random.randint(0, 2))
-        self.chance_for_pub_fight = round((self.mood_level / 30) * (self.number_of_people / 5))
+        self.chance_for_pub_fight = str(round((self.mood_level / 30) * (self.number_of_people / 5))) + "%"
 
     def handle_pub_fight(self):
         self.number_of_people = 0
