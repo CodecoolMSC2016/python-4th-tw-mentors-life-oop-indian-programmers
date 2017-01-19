c52331f0ae1463afbbbd4daefe189b59d182bdd6
imre_meszesan <imre.meszesan@gmail.com>
2017-01-19 15:06:46 +0100

    added string return values to instance methods

--- pub.py
+++ pub.py
@@ -3,12 +3,13 @@ import random
 
 class Pub():
 
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.mood_level = random.randint(0, 100)
         self.number_of_people = random.randint(1, 30)
-        self.music_playing = True
+        self.music_playing = False
         self.free_tables = bool(random.randint(0, 2))
-        self.chance_for_pub_fight = (self.mood_level / 30) * (self.number_of_people / 5)
+        self.chance_for_pub_fight = round((self.mood_level / 30) * (self.number_of_people / 5))
 
     def handle_pub_fight(self):
         self.number_of_people = 0
@@ -16,7 +17,12 @@ class Pub():
 
     def change_music(self):
         self.music_playing = True
+        music_list = []
+        with open("music_list.txt") as f:
+            for line in f:
+                music_list.append(line)
+        return music_list[random.randint(0, len(music_list))] + "is playing in the pub."
 
-    def drink_alcohol(self):
-        self.moodlevel += 5
-        return self.moodlevel
+    def serve_alcohol(self):
+        self.mood_level += 5
+        return "The overall mood increased in the pub " + str(self.mood_level)
