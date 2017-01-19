97d4d7455fc46430874ad24f742721661fbc7868
LewaDubz <lestrider@gmail.com>
2017-01-19 11:40:11 +0100

    Student.py fixed

--- student.py
+++ student.py
@@ -12,9 +12,10 @@ class Student(Person):
     @classmethod
     def create_by_csv(cls, csv_path):
         students = []
-        with open(csv_path, "r") as csvfile:
-            csv_file = csv.reader(csvfile)
-            for row in csv_file:
-                student = Student(row[0], row[1])
-                students.append(student)
-        return students
+        myfile = open(csv_path, "r")
+        for line in myfile:
+            first_name, last_name = line[0], line[1]
+            year_of_birth, gender, knowledge_level, energy_level = line[2], line[3], line[4], line[5]
+            mentors.append(Mentor(first_name, last_name, year_of_birth, gender, knowledge_level, energy_level))
+        myfile.close()
+        return mentors
