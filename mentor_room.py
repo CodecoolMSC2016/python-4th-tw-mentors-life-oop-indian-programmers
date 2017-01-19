4283516aa5259f9708f94dcaa245a346ad12470d
LewaDubz <lestrider@gmail.com>
2017-01-19 11:50:37 +0100

    Added mentor_room open_window class method

--- mentor_room.py
+++ mentor_room.py
@@ -1,6 +1,11 @@
 class Mentor_Room():
-    def __init__(self, bfa_running, is_clean, mood_level, window_open):
+    def __init__(self, bfa_running, is_clean=True, mood_level, window_open):
         self.bfa_running = bfa_running
         self.is_clean = is_clean
         self.mood_level = mood_level
         self.window_open = window_open
+        
+    def open_window(self):
+        if self.is_clean == False:
+            self.window_open = True
+            self.clean = True
