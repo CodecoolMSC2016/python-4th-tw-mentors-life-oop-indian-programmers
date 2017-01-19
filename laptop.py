59079e21d64bbc8d43fb380057ddf817597a3aad
imre_meszesan <imre.meszesan@gmail.com>
2017-01-19 10:31:33 +0100

    renamed temperature.py , added message

--- laptop.py
+++ laptop.py
@@ -17,14 +17,14 @@ class Laptop:
         if self.battery_level < 100:
             self.battery_level = 100
 
-    def _add_temperature(self, value):
+    def add_temperature(self, value):
         self.cpu_temp += value
         if self.cpu_temp > 85:
             raise ProcessorOverheat("CPU_TEMP reached " + str(self.cpu_temp) + "C.")
         elif self.cpu_temp > 60:
             return self.cpu_temp
 
-    def _gen_load_msg(self, message):
+    def gen_load_msg(self, message):
         for i in range(4):
             time.sleep(0.5)
             yield message + "." * i
@@ -34,41 +34,18 @@ class Laptop:
     def turn_on(self):
         if not self.is_power_on:
             self.is_power_on = True
-            self.cpu_temp = 45
-            self.battery_level = 0.4
-            for message in self._gen_load_msg("Loading"):
-                os.system("clear")
-                print(message)
-            self._run_REPL()
+            return self.gen_load_msg("Loading")
 
     def turn_off(self):
         if self.is_power_on:
-            self.is_power_on = False
-            self.cpu_temp = 0
-            for message in self._gen_load_msg("Shutdown"):
-                os.system("clear")
-                print(message)
-
-    def _run_code(self, code):
-        result = eval(code)
-        temperature = self._add_temperature(10)
-        if temperature:
-            print("Warning: cpu temp " + str(temperature) + "C")
-        if result:
-            print(result)
-
-    def _run_REPL(self):
-        print('''
-Python 3.6 (default, Sep 16 2015, 09:25:04)
-[GCC 4.8.2] on linux
-Type "help", "copyright", "credits" or "license" for more information.
-''', end="")
-        while True:
-            code = input(">>> ")
-            if code == "quit()":
-                self.turn_off()
-            self._run_code(code)
+            self.is_power_on = True
+            return self.gen_load_msg("Shutdown")
 
 
 laptop = Laptop()
-laptop.turn_on()
+for msg in laptop.turn_on():
+    os.system("clear")
+    print(msg)
+for msg in laptop.turn_off():
+    os.system("clear")
+    print(msg)
