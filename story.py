4e58744617d186ac8fe135b651a2b6ae92885b45
imre_meszesan <imre.meszesan@gmail.com>
2017-01-19 11:04:52 +0100

    story work in progress

--- story.py
+++ story.py
@@ -1,5 +1,17 @@
 from codecool_class import CodecoolClass
 from mentor import Mentor
 from student import Student
+from laptop import Laptop
+from pub import Pub
+from management import Management
+from mentor_room import Mentor_Room
+import os
 
-codecool_bp = CodecoolClass.create_local
+
+codecool_bp = CodecoolClass.create_local()
+
+input("Turn on laptop? ")
+laptop = Laptop()
+for msg in laptop.turn_on():
+    os.system("clear")
+    print(msg)
