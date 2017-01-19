0776ccfdf7f5fed37b403825ed067c4f7c8cf8d4
imre_meszesan <imre.meszesan@gmail.com>
2017-01-19 11:30:09 +0100

    created classmethod, added csv_path parameter

--- mentor.py
+++ mentor.py
@@ -3,13 +3,17 @@ from person import Person
 
 class Mentor(Person):
 
-    def __init__(self, nickname, has_contract=True, salary=200000):
+    def __init__(self, first_name, last_name, year_of_birth, gender, nickname, has_contract=True, salary=200000):
         super().__init__(first_name, last_name, year_of_birth, gender)
         self.nickname = nickname
 
-    def create_by_csv(csv_path):
-        students = []
+    @classmethod
+    def create_by_csv(cls, csv_path):
+        mentors = []
         myfile = open(csv_path, "r")
-        for lin in myfile:
-            student.append(line)
+        for line in myfile:
+            first_name, last_name = line[0], line[1]
+            year_of_birth, gender, nickname = line[2], line[3], line[4]
+            mentors.append(Mentor(first_name, last_name, year_of_birth, gender, nickname))
         myfile.close()
+        return mentors
